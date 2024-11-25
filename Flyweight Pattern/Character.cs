710e225b7031bbbc92cee5bbc0633931894b8bf4
magobul <magobul@gmail.com>
2024-11-25 11:56:33 +0700

    Add files via upload

---
+++ Flyweight Pattern/Character.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight_Pattern
+{
+    public class Character
+    {
+        public string Name { get; private set; }
+        public string Type { get; private set; }
+        public string Image { get; private set; }
+
+       
+        public int Level { get; set; }
+        public int Experience { get; set; }
+
+       
+        private Character(string name, string type, string image)
+        {
+            Name = name;
+            Type = type;
+            Image = image;
+            Level = 1; 
+            Experience = 0; 
+        }
+
+    
+        public class CharacterFactory
+        {
+            private Dictionary<string, Character> characters = new Dictionary<string, Character>();
+
+            public Character GetCharacter(string name, string type, string image)
+            {
+                string key = $"{name}:{type}"; 
+
+                if (!characters.ContainsKey(key))
+                {
+                    characters[key] = new Character(name, type, image);
+                }
+
+                return characters[key];
+            }
+        }
+    }
+}
+
---
+++ Flyweight Pattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using Flyweight_Pattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new Character.CharacterFactory();
+
+        // Создание персонажей
+        var character1 = factory.GetCharacter("Герой", "Воин", "warrior.png");
+        var character2 = factory.GetCharacter("Герой", "Воин", "warrior.png"); // Возвращает существующий экземпляр
+
+        // Изменение атрибутов
+        character1.Level = 5;
+        character1.Experience = 100;
+
+        Console.WriteLine($"Персонаж 1: {character1.Name}, Тип: {character1.Type}, Уровень: {character1.Level}, Опыт: {character1.Experience}");
+        Console.WriteLine($"Персонаж 2: {character2.Name}, Тип: {character2.Type}, Уровень: {character2.Level}, Опыт: {character2.Experience}");
+
+        // Проверка, что оба объекта ссылаются на один и тот же экземпляр
+        Console.WriteLine($"Оба объекта ссылаются на один и тот же экземпляр? {ReferenceEquals(character1, character2)}");
+    }
+}
